21c9771e29312b611200a5b79985dcb587e96be6
ShocKKKKKKK <sergey.zhmursky@gmail.com>
2020-04-27 10:37:41 +0300

    added header and footer adaptive

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,8 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+$(function () {
+  $('.nav__menu-btn').on('click', function () {
+    $('.nav__list').slideToggle();
+  });
 
-pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+
 });
