86b8762c48860e1de56b18276999811fe9333ee9
ShocKKKKKKK <sergey.zhmursky@gmail.com>
2020-05-07 17:00:44 +0300

    .

--- build/js/script.js
+++ build/js/script.js
@@ -1,14 +1,14 @@
-var navMain = document.querySelector('.main-nav');
+var navMain = document.querySelector('.nav__list');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('nav__list--closed')) {
+    navMain.classList.remove('nav__list--closed');
+    navMain.classList.add('nav__list--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('nav__list--closed');
+    navMain.classList.remove('nav__list--opened');
   }
 });
